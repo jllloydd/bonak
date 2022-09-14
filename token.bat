52b3cb92b838f10ab5eb87072e7cf810a5c0aae2
jllloydd <wayfivet@gmail.com>
2022-09-14 15:07:48 +0800

    Auto Update

--- token.bat
+++ token.bat
@@ -1,1 +1,1 @@
-git remote set-url origin https://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@github.com/jllloydd/bonak.git
+git remote set-url origin https://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.com/jllloydd/bonak.git
