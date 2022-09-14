dc9a7c810e0c446abd99a446c1ea8fa80bdeb80c
jllloydd <wayfivet@gmail.com>
2022-09-14 14:44:46 +0800

    Auto Update

--- push.bat
+++ push.bat
@@ -1,4 +1,15 @@
 @echo off
+git config --global user.email "wayfivet@gmail.com"
+git config --global user.name "jllloydd"
+
+pause
+
+git remote set-url origin https://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@github.com/jllloydd/bonak.git
+
+pause
+
 git add .
 git commit -m "Auto Update"
 git push
+
+pause
